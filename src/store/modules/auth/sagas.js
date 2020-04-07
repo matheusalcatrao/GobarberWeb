0a8212882fad5c889020fb6c1f235b6f20e3ae2d
matheusalcatrao <matheusalcatrao@hotmail.com>
2020-04-07 13:23:01 -0300

    saga and service

---
+++ src/store/modules/auth/sagas.js
@@ -0,0 +1,25 @@
+import { takeLatest, call, put, all } from "redux-saga/effects";
+
+import api from "~/services/api";
+
+import { signInSuccess } from "./actions";
+
+export function* signIn({ payload }) {
+    const { email, password } = payload;
+
+    const response = yield call(api.post, "sessions", {
+        email,
+        password
+    });
+
+    const { token, user } = response.data;
+
+    if (!user.provider) {
+        console.tron.log("erro");
+        return;
+    }
+
+    yield put(signInSuccess(token, user));
+}
+
+export default all([takeLatest("@auth/SIGN_IN_REQUEST", signIn)]);
