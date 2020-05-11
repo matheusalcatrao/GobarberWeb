8ad3da4de7c1de961974c6b05cdbee1673c502a8
Matheus Alcatrão <matheusalcatrao@hotmail.com>
2020-05-11 18:15:42 -0300

    Notification

--- src/components/Notifications/index.js
+++ src/components/Notifications/index.js
@@ -1,6 +1,9 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
 import { MdNotifications } from "react-icons/md";
+import { parseISO, formatDistance } from "date-fns";
+import pt from "date-fns/locale/pt";
+
+import api from "~/services/api";
 
 import {
     Container,
@@ -11,12 +14,38 @@ import {
 } from "./styles";
 
 export default function Notifications() {
+    const [visible, setVisible] = useState(false);
+    const [notifications, setNotifications] = useState([]);
+
+    useEffect(() => {
+        async function loadingNotification() {
+            const response = await api.get("notifications");
+
+            const data = response.data.map(notification => ({
+                ...notification,
+                timeDistance: formatDistance(
+                    parseISO(notification.createdAt),
+                    new Date(),
+                    { addSuffix: true, locale: pt }
+                )
+            }));
+
+            setNotifications(data);
+        }
+
+        loadingNotification();
+    }, []);
+
+    function handleChangeVisible() {
+        setVisible(!visible);
+    }
+
     return (
         <Container>
-            <Badge hasUnread>
+            <Badge onClick={handleChangeVisible} hasUnread>
                 <MdNotifications color="#7159c1" size={20} />
             </Badge>
-            <NotificationList>
+            <NotificationList visible={visible}>
                 <Scroll>
                     <Notification unread>
                         <p>Você possui um novo agendamento para amanhã</p>
